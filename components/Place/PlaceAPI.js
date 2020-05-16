1bff4d06a2d1bae5fbf9f29401bd1ccba732942c
memolokokj <62449690+memolokokj@users.noreply.github.com>
2020-05-15 18:54:33 -0600

    ExportarTablas
    mandamos los datos al mapa para que los ubique

--- components/Place/PlaceAPI.js
+++ components/Place/PlaceAPI.js
@@ -5,6 +5,9 @@
 const express = require("express");
 const cors = require("cors");
 const services = require("./Services");
+const lugaresDB = require("./PlaceDAL");
+
+
 
 const Place = express.Router();
 const middleware = require("../Middleware");
@@ -12,10 +15,36 @@ Place.use(express.urlencoded({ extended: false }));
 Place.use(express.json());
 Place.use(cors());
 
-Place.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Metodo get"
-  });
+
+
+const lugares = [];
+lugaresDB.findAll(
+  {attributes: ['name', 'image', 'lat', 'lon', 'description']}
+).then(projects => {
+  for(var i=0;i<projects.length;i++)
+    lugares.push(projects[i].dataValues);
+})
+
+Place.get("/", (req, res) =>{
+    const response={
+      data: [],
+      message: []  
+    }
+    let statusCode = null;
+    const {search}= req.query;
+    if(search && search.trim()){
+        //filtrar en el JSON
+        response.data=lugares.filter(lugar =>{
+            //si retorna true
+            return lugar.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+        });
+        statusCode=200;
+    }else{
+        //ENVIAR TODO LOS REGISTROS(JSON)
+        response.data=lugares;
+        statusCode=200;
+    }
+    res.status(statusCode).send(response);
 });
 
 Place.post("/", middleware, (req, res) => {
--- seeders/01_20200424080207-Places.js
+++ seeders/01_20200424080207-Places.js
@@ -12,18 +12,18 @@ module.exports = {
       [
         {
           name: "UABCS",
-          image: "",
-          lat: "24.105836",
-          lon: "-110.320266",
-          description: "Universidad en cuarentena",
+          image: "https://elinformantebcs.mx/wp-content/uploads/2016/08/elinformantebcs.mx_uabcs-1.jpg",
+          lat: "24.102744",
+          lon: "-110.315969",
+          description: "La Universidad Autónoma de Baja California Sur (UABCS) es una Institución pública que ofrece programas educativos de calidad.",
           userId: 1,
         },
         {
-          name: "Galerias la paz",
-          image: "",
-          lat: "24.1175948",
-          lon: "-110.3376689",
-          description: "Centro comercial en cuarentena",
+          name: "TEC",
+          image: "https://www.diarioelindependiente.mx/noticias/2018/10/original/153969314933905.jpg",
+          lat: "24.119680",
+          lon: "-110.309086",
+          description: "El Instituto Tecnológico de La Paz es una institución pública de educación superior localizada en La Paz, Baja California Sur. Es considerada como la primera institución educativa de nivel licenciatura en el Estado de Baja California Sur.",
           userId: 1,
         },
       ], {});
