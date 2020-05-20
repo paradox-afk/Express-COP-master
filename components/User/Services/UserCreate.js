8b004468fa5fe08d487a3df5ea797e25ef6ea661
avremigue17 <62452007+avremigue17@users.noreply.github.com>
2020-05-20 01:38:43 -0600

    Update UserCreate.js

--- components/User/Services/UserCreate.js
+++ components/User/Services/UserCreate.js
@@ -1,29 +1,68 @@
 const dal = require("../UserDAL");
 const {hashPassword} = require("../../../libs/utils");
-const nodemon = require("nodemon");
 
 const usuarios = [];
 const usuariosDB = require("../UserDAL");
-var palanca = false;
-
-usuariosDB.findAll(
-  {attributes: ['correo']}
-).then(projects => {
-  for(var i=0;i<projects.length;i++)
-    usuarios.push(projects[i].dataValues);
-})
 
 
 module.exports = ({nombre, ap_paterno, ap_materno, correo, usuario, contraseña, fecha_nacimiento}, res) => 
 {
-    
-    for(var i=0;i<usuarios.length;i++)
+    var usuarios = [];
+    var palanca = false;
+
+    usuariosDB.findAll(
+        {attributes: ['correo']}
+      ).then(projects => {
+        for(var i=0;i<projects.length;i++){
+            if(projects[i].dataValues.correo==correo){
+                palanca = true;
+            }
+        }
+        insertar(nombre, ap_paterno, ap_materno, correo, usuario, contraseña, fecha_nacimiento,palanca,res);
+      });
+      
+    /*for(var i=0;i<usuarios.length;i++)
     {
         console.log(usuarios[i]);
-        if(usuarios[i].correo == correo)
+        if(usuarios[i] == correo)
             palanca = true;
+    }*/
+    /*
+    if(!palanca)
+    {
+        contraseña = hashPassword(contraseña);
+        if(nombre && ap_paterno && ap_materno && correo && usuario && contraseña && fecha_nacimiento) {
+            dal.create(
+            {
+                nombre, 
+                ap_paterno, 
+                ap_materno, 
+                correo, 
+                usuario,
+                contraseña,
+                fecha_nacimiento,
+            });
+            res.status(200).json({
+                message: "Usuario creado correctamente",
+            });
+        }
+        else
+        {
+            res.status(400).json(
+            {
+                message: "No se enviaron correctamente los parametros.",
+                parameters: ["nombre", "ap_paterno", "ap_materno", "correo", "usuario", "contraseña", "fecha_nacimiento"],
+            });
+        }
     }
-    
+    else{
+        res.status(200).json(
+            {
+                message: "Correo utilizado.",
+            });
+    }*/
+};
+function insertar (nombre, ap_paterno, ap_materno, correo, usuario, contraseña, fecha_nacimiento,palanca,res){
     if(!palanca)
     {
         contraseña = hashPassword(contraseña);
@@ -57,4 +96,4 @@ module.exports = ({nombre, ap_paterno, ap_materno, correo, usuario, contraseña,
                 message: "Correo utilizado.",
             });
     }
-};
+}
