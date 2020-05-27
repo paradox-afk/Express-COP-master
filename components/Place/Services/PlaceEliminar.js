18fad4e1fbce712b24a2091b1cea7b938a7bedf5
memolokokj <62449690+memolokokj@users.noreply.github.com>
2020-05-26 18:42:33 -0600

    reparacion de eliminacion
    reparamos unos problemas que tuvimos con las llaves foraneas

--- components/Place/Services/PlaceEliminar.js
+++ components/Place/Services/PlaceEliminar.js
@@ -1,8 +1,14 @@
 const dal = require("../PlaceDAL");
+const dal2 = require("../../Calificaciones/CalificacionesDAL");
 
 module.exports = ({id}, res) => {
   console.log(id);
 
+  dal2.destroy({ 
+    where: { 
+     placeId:id,
+    } 
+  });
   dal.destroy({ 
     where: { 
      id:id,
